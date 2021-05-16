3da59cdd852fcf5e32482e413d8e0807be9d3aad
Johannes Meintrup <johannes.meintrup@mni.thm.de>
2021-05-16 09:39:24 +0200

    tabu

--- src/meta_heuristics/tabu_local_search.rs
+++ src/meta_heuristics/tabu_local_search.rs
@@ -3,13 +3,14 @@ use crate::heuristic_elimination_order::{
     EliminationOrderDecomposer, PermutationDecompositionResult,
 };
 use crate::solver::{AtomSolver, Bounds, ComputationResult};
-use crate::tree_decomposition::TreeDecomposition;
 use fxhash::{FxHashMap, FxHashSet};
+use rand::prelude::StdRng;
+use rand::{Rng, SeedableRng};
 use std::cmp::max;
 
 const DEFAULT_EPOCHS: usize = 10;
 const DEFAULT_STEPS: usize = 10;
-const DEFAULT_SEED: usize = 1337;
+const DEFAULT_SEED: u64 = 1337;
 const DEFAULT_MAX_TABU: usize = 7;
 
 pub struct TabuLocalSearch {
@@ -19,18 +20,21 @@ pub struct TabuLocalSearch {
     best: Option<PermutationDecompositionResult>,
     upperbound: usize,
     lowerbound: usize,
-    seed: usize,
-    tabu: Vec<usize>,
+    seed: u64,
     max_tabu_size: usize,
+}
+
+fn permutation_map(permutation: &[usize]) -> FxHashMap<usize, usize> {
+    permutation
+        .iter()
+        .enumerate()
+        .map(|(i, v)| (*v, i))
+        .collect()
 }
 
 impl TabuLocalSearch {
     fn fitness(&self, permutation: &[usize]) -> f64 {
-        let vertex_to_position: FxHashMap<_, _> = permutation
-            .iter()
-            .enumerate()
-            .map(|(i, v)| (*v, i))
-            .collect();
+        let vertex_to_position = permutation_map(permutation);
 
         let mut working_graph = self.graph.clone();
 
@@ -50,14 +54,14 @@ impl TabuLocalSearch {
                 .copied()
                 .collect();
             max_bag = max(max_bag, bag.len());
-            fitness += bag.len() * bag.len();
+            fitness += bag.len().pow(2) as f64;
             for a in &bag {
                 for b in bag.iter().filter(|b| a < *b) {
                     working_graph.add_edge(*a, *b);
                 }
             }
         }
-        fitness + max_bag.pow(2) * m.pow(2)
+        fitness + max_bag.pow(2) as f64 * m.pow(2) as f64
     }
 
     pub fn new(graph: HashMapGraph) -> Self {
@@ -70,7 +74,6 @@ impl TabuLocalSearch {
             upperbound,
             lowerbound: 0,
             seed: DEFAULT_SEED,
-            tabu: Default::default(),
             max_tabu_size: DEFAULT_MAX_TABU,
         }
     }
@@ -100,14 +103,20 @@ impl TabuLocalSearch {
         self
     }
 
-    pub fn seed(mut self, seed: usize) -> Self {
+    pub fn seed(mut self, seed: u64) -> Self {
         self.seed = seed;
         self
     }
 
     pub fn best_permutation(mut self, best_permutation: PermutationDecompositionResult) -> Self {
-        self.best_permutation = best_permutation;
+        self.best = Option::from(best_permutation);
         self
+    }
+
+    fn swap(&self, permutation: &[usize], v_pos: usize, u_pos: usize) -> Vec<usize> {
+        let mut tmp = Vec::from(permutation);
+        tmp.swap(v_pos, u_pos);
+        tmp
     }
 }
 
@@ -129,50 +138,131 @@ impl AtomSolver for TabuLocalSearch {
     }
 
     fn compute(mut self) -> ComputationResult {
-        if self.best_permutation.is_none() {
-            self.best_permutation = EliminationOrderDecomposer::new(
-                self.graph.clone(),
-                self.graph.vertices().collect(),
-            )
-            .compute();
+        assert!(self.max_tabu_size + 2 < self.graph.order());
+
+        let mut rng: StdRng = SeedableRng::seed_from_u64(self.seed);
+        if self.best.is_none() {
+            self.best = Option::from(
+                EliminationOrderDecomposer::new(
+                    self.graph.clone(),
+                    self.graph.vertices().collect(),
+                )
+                .compute(),
+            );
         }
 
-        for epoch in 0..self.epochs {
+        let mut eval_opt = self.fitness(&self.best.as_ref().unwrap().permutation);
+        let mut permutation = self.best.as_ref().unwrap().permutation.clone();
+        let mut tabu: Vec<usize> = vec![];
+        for _ in 0..self.epochs {
+            let tmp =
+                EliminationOrderDecomposer::new(self.graph.clone(), permutation.clone()).compute();
 
-            let mut working_permutation;
-            let mut working_bag_map;
-            let mut working_td;
-            if epoch == 0 {
-                let tmp = self.best.as_ref().unwrap();
-                working_permutation = tmp.permutation.clone();
-                working_bag_map = tmp.eliminated_in_bag.clone();
-                working_td = tmp.tree_decomposition.clone();
-            } else {
-                let tmp = EliminationOrderDecomposer::new(
-                    self.graph.clone(),
-                    working_permutation.unwrap().clone(),
-                )
-                .compute();
-                working_permutation = tmp.permutation.clone();
-                working_bag_map = tmp.eliminated_in_bag.clone();
-                working_td = tmp.tree_decomposition.clone();
+            let mut map = tmp.eliminated_in_bag;
+            let mut pos = permutation_map(&permutation);
+            let mut td = tmp.tree_decomposition;
+            let mut eval = self.fitness(&permutation);
+
+            for _ in 0..self.steps {
+                let mut best_neighbor_perm: Option<Vec<usize>> = None;
+                let mut best_neighbor: Option<usize> = None;
+                let mut eval_tmp = f64::MAX;
+
+                for v in &permutation {
+                    if !tabu.contains(v) {
+                        let mut minw = None;
+                        let mut min = *pos.get(v).unwrap();
+                        let mut maxw = None;
+                        let mut max = *pos.get(v).unwrap();
+
+                        for w in &td.bags[*map.get(v).unwrap()].vertex_set {
+                            let pw = *pos.get(w).unwrap();
+                            if pw < min {
+                                minw = Some(w);
+                                min = pw;
+                            }
+                            if pw > max {
+                                maxw = Some(w);
+                                max = pw;
+                            }
+                        }
+
+                        if let Some(maxw) = maxw {
+                            let perm_max = self.swap(&permutation, *pos.get(v).unwrap(), max);
+                            let eval_max = self.fitness(&permutation);
+                            if eval_max < eval_tmp {
+                                eval_tmp = eval_max;
+                                best_neighbor_perm = Some(perm_max);
+                                best_neighbor = Some(*maxw);
+                            }
+                        }
+                        if let Some(minw) = minw {
+                            let perm_min = self.swap(&permutation, *pos.get(v).unwrap(), min);
+                            let eval_min = self.fitness(&permutation);
+                            if eval_min < eval_tmp {
+                                eval_tmp = eval_min;
+                                best_neighbor_perm = Some(perm_min);
+                                best_neighbor = Some(*minw);
+                            }
+                        }
+                    }
+                }
+
+                if eval_tmp < eval {
+                    permutation = best_neighbor_perm.unwrap();
+                    let tmp =
+                        EliminationOrderDecomposer::new(self.graph.clone(), permutation.clone())
+                            .compute();
+                    map = tmp.eliminated_in_bag;
+                    pos = permutation_map(&permutation);
+                    td = tmp.tree_decomposition;
+                    eval = self.fitness(&permutation);
+
+                    tabu.push(best_neighbor.unwrap());
+                    if tabu.len() > self.max_tabu_size {
+                        tabu.remove(0);
+                    }
+                } else {
+                    break;
+                }
             }
 
+            if eval < eval_opt {
+                let tmp = EliminationOrderDecomposer::new(self.graph.clone(), permutation.clone())
+                    .compute();
+                if tmp.tree_decomposition.max_bag_size
+                    < self.best.as_ref().unwrap().tree_decomposition.max_bag_size
+                {
+                    self.best = Some(tmp);
+                    eval_opt = eval;
+                }
+            }
 
-            let mut working_fitness = self.fitness(&working_permutation);
-            let vertex_to_position: FxHashMap<_, _> = working_permutation
-                .iter()
-                .enumerate()
-                .map(|(i, v)| (*v, i))
-                .collect();
+            let mut choices: FxHashSet<_> = permutation.iter().copied().collect();
+            for v in &tabu {
+                choices.remove(v);
+            }
+            assert!(choices.len() > 1);
+            let mut choices: Vec<_> = choices.iter().copied().collect();
+            let i = rng.gen_range(0, choices.len());
+            let v = choices[i];
+            let v_pos = *pos.get(&v).unwrap();
+            choices.swap_remove(i);
 
+            let j = rng.gen_range(0, choices.len());
+            let u = choices[j];
+            let u_pos = *pos.get(&u).unwrap();
 
-
+            permutation = self.swap(&permutation, v_pos, u_pos);
         }
-
-        ComputationResult::Bounds(Bounds {
-            upperbound: self.upperbound,
-            lowerbound: self.lowerbound,
-        })
+        let td = self.best.unwrap().tree_decomposition;
+        if td.max_bag_size - 1 < self.upperbound {
+            ComputationResult::ComputedTreeDecomposition(td)
+        } else {
+            ComputationResult::Bounds(Bounds {
+                lowerbound: self.lowerbound,
+                upperbound: self.upperbound,
+            })
+        }
     }
 }
